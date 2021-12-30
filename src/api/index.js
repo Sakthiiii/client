88f1ca3c94060c6c7b188668bd4d5868fad85b23
Sakthi <87663226+Sakthiiii@users.noreply.github.com>
2021-12-30 20:44:43 +0530

    first

--- src/api/index.js
+++ src/api/index.js
@@ -21,15 +21,21 @@ export const sendEmail = (token, form) => axios.post(`${url}/email/send`, form, 
 export const saveDraft = (token, form) => axios.post(`${url}/email/draft`, form, headers(token));
 export const updateDraft = (token, id, form) =>
   axios.put(`${url}/email/draft/${id}`, form, headers(token));
+
 export const moveToTrash = (token, id) =>
   axios.put(`${url}/email/${id}/trash`, null, headers(token));
+
 export const removeFromTrash = (token, id) =>
   axios.put(`${url}/email/${id}/untrash`, null, headers(token));
+
 export const markAsRead = (token, id) => axios.put(`${url}/email/${id}/read`, null, headers(token));
 export const markAsUnread = (token, id) =>
   axios.put(`${url}/email/${id}/unread`, null, headers(token));
+
 export const setFavorite = (token, id) =>
   axios.put(`${url}/email/${id}/favorite`, null, headers(token));
+
 export const unsetFavorite = (token, id) =>
   axios.put(`${url}/email/${id}/unfavorite`, null, headers(token));
+  
 export const deleteEmail = (token, id) => axios.delete(`${url}/email/${id}`, headers(token));
